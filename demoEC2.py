59d78e390318d330143a737a47d63aa4a3757143
rfrenier <rfrenier@gmail.com>
2016-06-28 13:41:44 -0400

    Add files via upload

--- demoEC2.py
+++ demoEC2.py
@@ -9,7 +9,7 @@ s3template = "https://s3.amazonaws.com/rf-cf-fidelity/web-cf.json"
  # Run CFN command
 cfnconn = boto.cloudformation.connect_to_region("us-east-1")
 print "\nCreating stack...\n"
-cfnoutput = cfnconn.create_stack("rf-test01", template_url=s3template)
+cfnoutput = cfnconn.create_stack("rf-test02", template_url=s3template)
 if "arn:aws:cloudformation:" not in cfnoutput:
 	print "Stack create failed."
 	sys.exit(2)
