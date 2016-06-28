2d9413aa788920559a9d3188dee31a02beca7d8c
rfrenier <rfrenier@gmail.com>
2016-06-28 14:11:44 -0400

    Add files via upload

--- demoEC2.py
+++ demoEC2.py
@@ -2,6 +2,52 @@
 import boto
 import boto.cloudformation
 
+def main():
+	# Define arguement variables
+	app = ""
+	nvtype = ""
+	env = ""
+	ver = ""
+	dnsuser = ""
+	dnspass = ""
+	owner = ""
+	email = ""
+
+
+
+
+	# Grab parameters and populate variables
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"ha:t:n:v:u:p:o:e:",["app=","type=","env=","ver=","dnsuser=","dnspass=","owner=","email="])
+	except getopt.GetoptError: 
+		print helpmessage
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print helpmessage
+			sys.exit(2)
+		elif opt in ("-a", "--app"):
+			app = arg
+		elif opt in ("-t", "--type"):
+			nvtype = arg
+		elif opt in ("-n", "--env"):
+			env = arg
+		elif opt in ("-v", "--ver"):
+			ver = arg
+		elif opt in ("-u", "--dnsuser"):
+			dnsuser = arg
+		elif opt in ("-p", "--dnspass"):
+			dnspass = arg
+		elif opt in ("-o", "--owner"):
+			owner = arg
+		elif opt in ("-e", "--email"):
+			email = arg
+		else:
+			print helpmessage
+			sys.exit(2)
+
+# Define stackname
+	stackname = app 
 
  # Set S3 Template URL
 s3template = "https://s3.amazonaws.com/rf-cf-fidelity/web-cf.json"
@@ -9,7 +55,7 @@ s3template = "https://s3.amazonaws.com/rf-cf-fidelity/web-cf.json"
  # Run CFN command
 cfnconn = boto.cloudformation.connect_to_region("us-east-1")
 print "\nCreating stack...\n"
-cfnoutput = cfnconn.create_stack("rf-test02", template_url=s3template)
+cfnoutput = cfnconn.create_stack(stackname, template_url=s3template)
 if "arn:aws:cloudformation:" not in cfnoutput:
 	print "Stack create failed."
 	sys.exit(2)
