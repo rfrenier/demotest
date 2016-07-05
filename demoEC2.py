cca8292c95d76283011f0e25cec60500f88d0083
rfrenier <rfrenier@gmail.com>
2016-07-05 13:42:44 -0400

    Add files via upload

--- demoEC2.py
+++ demoEC2.py
@@ -72,6 +72,9 @@ def main():
 	# Build CFN parameters
 	cfecho = ""
 	cfnparams = diction["cfn_inputs"].strip().split(",")
+	
+	print "\nPrinting diction\n" + diction
+	
 	for detail in diction["echo2cfdetails"].strip().split(","):
 		cfecho = cfecho + "echo " + detail + " >> /tmp/cf_details.txt;"
 	paramslist = []
