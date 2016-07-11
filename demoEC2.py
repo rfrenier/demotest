8922e2572c7c6dd6d6b076f2d9b792429994092c
rfrenier <rfrenier@gmail.com>
2016-07-11 19:21:45 -0400

    Add files via upload

--- demoEC2.py
+++ demoEC2.py
@@ -63,13 +63,13 @@ def main():
 	#	sys.exit(2)
 
 	# Define stackname
-	stackname = app + "-" + nvtype
+	stackname = app + "-" + nvtype + "-" + time.strftime("%H%M%S")
 	#+ "-" + nvtype + "-" + env + "-" + ver + "-" + time.strftime("%H%M%S")
 
 	
 	# Pull Chef Environment default_attributes
 	chefpull = "/usr/bin/knife environment show -a default_attributes " + app + "_" + nvtype 
-	#+ "_" + nvtype + "_" + env + "_" + ver + " -c " + knifefile + " | sed 1,2d"
+	# + "_" + env + "_" + ver + " -c " + knifefile + " | sed 1,2d"
 	print "\nPulling chef environment:\n" + chefpull
 	defattrib = commands.getoutput(chefpull)
 	print "\nOutput:\n" + defattrib
